8923140da0d16a8929bc7c998d49d05a0abf406f
Jan Müller <jpmueller@gmx.net>
2013-11-23 14:14:48 +0100

    added bootstrap luftmensch theme (based on cosmo)

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,7 +19,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 THEME = 'pelican-bootstrap3'
-BOOTSTRAP_THEME = 'cosmo'
+BOOTSTRAP_THEME = 'luftmensch'
 
 STATIC_PATHS = (
     'images',
