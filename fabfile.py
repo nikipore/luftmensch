0c74cfd1eec20e30b4f9204c39497ea0b8003ffe
Jan Müller <jpmueller@gmx.net>
2013-12-03 20:00:05 +0100

    brand icon added

--- fabfile.py
+++ fabfile.py
@@ -3,44 +3,46 @@ import fabric.contrib.project as project
 import os
 
 # Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'output'
+env.build_path = 'output'
 env.content_path = 'content'
+env.dist_path = 'dist'
 
 # Remote server configuration
 production = 'nikipore@pavo.uberspace.de:22'
 dest_path = '/var/www/virtual/nikipore/html/'
 
 def clean():
-    if os.path.isdir(env.deploy_path):
-        local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
+    for path in (env.build_path, env.dist_path):
+        if os.path.isdir(path):
+            local('rm -rf {0}'.format(os.path.join(path, '*')))
+        local('mkdir {0}'.format(path))
 
 def build():
-    local('pelican -s pelicanconf.py {content_path}'.format(**env))
+    local('pelican -s pelicanconf.py -o {build_path} {content_path}'.format(**env))
 
 def rebuild():
     clean()
     build()
 
 def regenerate():
-    local('pelican -r -s pelicanconf.py {content_path}'.format(**env))
+    local('pelican -r -s pelicanconf.py -o {build_path} {content_path}'.format(**env))
 
 def serve():
-    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+    local('cd {build_path} && python -m SimpleHTTPServer'.format(**env))
 
 def reserve():
     build()
     serve()
 
-def preview():
-    local('pelican -s publishconf.py {content_path}'.format(**env))
+def dist():
+    local('pelican -s publishconf.py -o {dist_path} {content_path}'.format(**env))
 
 @hosts(production)
 def publish():
-    preview()
+    dist()
     project.rsync_project(
-        remote_dir=dest_path,
+        remote_dir=dest_path.rstrip('/') + '/',
         exclude=".DS_Store",
-        local_dir=env.deploy_path.rstrip('/') + '/',
+        local_dir=env.dist_path.rstrip('/') + '/',
         delete=True
     )
