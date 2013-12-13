3bec7cae5b24abe9095cdabf4f8cd5205f2f7be9
Jan Müller <jpmueller@gmx.net>
2013-12-13 20:20:59 +0100

    sitemap in publish config

--- publishconf.py
+++ publishconf.py
@@ -15,3 +15,19 @@ SITEURL = 'http://luftmensch.net'
 ADDTHIS_PROFILE = 'luftmensch'
 DISQUS_SITENAME = 'luftmensch'
 GOOGLE_ANALYTICS = 'UA-40656153-1'
+
+PLUGINS += ('sitemap', )
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
